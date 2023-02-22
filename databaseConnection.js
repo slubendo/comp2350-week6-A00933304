f2509e9a635c971f71a2697beca539a204aedc74
slubendo <stephane.lubendo@gmail.com>
2023-02-22 12:47:38 -0800

    second commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+	host: "sql.freedb.tech",
+	user: "freedb_2350_mai",
+	password: "QF&#t#tYSbC!5J3",
+	database: "freedb_comp2350-week2-A00933304",
+	multipleStatements: false,
 };
 
 const dbConfigLocal = {
--- routes/router.js
+++ routes/router.js
@@ -19,4 +19,43 @@ router.get('/', async (req, res) => {
 	}
 });
 
+router.post('/addUser', async (req, res) => {
+	console.log("form submit");
+	console.log(req.body);
+   try {
+   const success = await dbModel.addUser(req.body);
+   if (success) {
+   res.redirect("/");
+   }
+   else {
+   res.render('error', {message: "Error writing to MySQL"});
+   console.log("Error writing to MySQL");
+   }
+   }
+   catch (err) {
+   res.render('error', {message: "Error writing to MySQL"});
+   console.log("Error writing to MySQL");
+   console.log(err);
+   }
+   });
+   
+
+   router.get('/deleteUser', async (req, res) => {
+	console.log("delete user");
+   console.log(req.query);
+   let userId = req.query.id;
+   if (userId) {
+   const success = await dbModel.deleteUser(userId);
+   if (success) {
+   res.redirect("/");
+   }
+   else {
+   res.render('error', {message: 'Error writing to MySQL'});
+   console.log("Error writing to mysql");
+   console.log(err);
+   }
+   }
+   });
+   
+
 module.exports = router;
